8b45720851bcfb4bfe9031355feb672d75a7a788
federicotempo <federicotempo98@gmail.com>
2024-01-30 22:14:25 -0300

    Se finaliza tarea 4
    Se termina de escribir el script de js y acomodar html. Tarea lista para subir

--- tarea4/tarea4.js
+++ tarea4/tarea4.js
@@ -37,6 +37,75 @@ function promedio(numeros) {
   }
 
   return sumaNumeros / cantidadNumeros;
-} 
+}
 
-generarLista()
+function encontrarNumeroMasPequenio(numeros) {
+  let numeroPequenio = numeros[0];
+  for (i = 1; i < numeros.length; i++) {
+    if (numeroPequenio > numeros[i]) {
+      numeroPequenio = numeros[i];
+    }
+  }
+  return numeroPequenio;
+}
+
+function encontrarNumeroMasGrande(numeros) {
+  let numeroGrande = numeros[0];
+  for (i = 1; i < numeros.length; i++) {
+    if (numeroGrande < numeros[i]) {
+      numeroGrande = numeros[i];
+    }
+  }
+  return numeroGrande;
+}
+
+function encontrarNumeroMasFrecuente(numeros) {
+  let frecuencia = {};
+  let numeroMasFrecuente;
+  let maxFrecuencia = 0;
+
+  for (i = 0; i < numeros.length; i++) {
+    let numero = numeros[i];
+
+    if (!frecuencia[numero]) {
+      frecuencia[numero] = 1;
+    } else {
+      frecuencia[numero]++;
+    }
+  }
+
+  for (let numero in frecuencia) {
+    if (frecuencia.hasOwnProperty(numero)) {
+      if (frecuencia[numero] > maxFrecuencia) {
+        maxFrecuencia = frecuencia[numero];
+        numeroMasFrecuente = numero;
+      }
+    }
+  }
+
+  if (maxFrecuencia === 1){
+    return "Ninguno";
+  }
+
+  return numeroMasFrecuente;
+}
+
+function mostrarEnHtml(numeroPromedio, numeroPequenio, numeroGrande, numeroFrecuente) {
+  document.querySelector("#promedio").innerText = `El promedio es: ${numeroPromedio}`;
+  document.querySelector("#pequeño").innerText = `El más pequeño es: ${numeroPequenio}`;
+  document.querySelector("#grande").innerText = `El más grande es: ${numeroGrande}`;
+  document.querySelector("#frecuente").innerText = `El más frecuente es: ${numeroFrecuente}`;
+}
+
+function main() {
+  generarLista();
+  const numeros = convertirLista();
+  let numeroPromedio = promedio(numeros);
+  let numeroPequenio = encontrarNumeroMasPequenio(numeros);
+  let numeroGrande = encontrarNumeroMasGrande(numeros);
+  let numeroFrecuente = encontrarNumeroMasFrecuente(numeros);
+  mostrarEnHtml(numeroPromedio, numeroPequenio, numeroGrande, numeroFrecuente);
+
+}
+
+main();
